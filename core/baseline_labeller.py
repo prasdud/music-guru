fc2cf9714ac0eebebef7bf85e4498b4b3314685d
prasdud <prasdudwastaken@gmail.com>
2025-08-08 15:43:00 +0530

    feat: working on baseline_labeller.py     i will forget about internal and multisyllabic rhyme scheme and     focus on just the MVP now

---
+++ core/baseline_labeller.py
@@ -0,0 +1,96 @@
+import json
+
+
+### -------------------------------
+# 1. Load JSONL Dataset
+### -------------------------------
+def load_jsonl(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        return [json.loads(line) for line in f]
+
+
+
+### -------------------------------
+# 2. Collect Last Words
+### -------------------------------
+def collect_last_words(song):
+    last_words = []
+    for line in song['lines']:
+        if not line['words']:
+            continue
+        last_word = line['words'][-1]
+
+        tempdict = {
+            "text": last_word['text'],
+            "phonemes": last_word['phonemes'],
+            "syllables": last_word['syllables'],
+            "stress": last_word['stress'],
+            "rhyme_id": line['rhyme_id'],
+        }
+
+        last_words.append(tempdict)
+    return last_words
+
+        
+
+### -------------------------------
+# 3. Generate Pairs
+### -------------------------------
+def generate_pairs(last_words):
+    
+
+### -------------------------------
+# 4. Compute Features
+### -------------------------------
+def compute_features(word1, word2):
+    
+
+### -------------------------------
+# 5. Store in List
+### -------------------------------
+def store_in_list():
+    
+
+
+### -------------------------------
+# 6. Save to CSV
+### -------------------------------
+def save_to_csv(pairs, output_path):
+    
+
+
+### -------------------------------
+# 7. Check Balance
+### -------------------------------
+def check_balance():
+    
+
+### -------------------------------
+# 8. Main Pipeline
+### -------------------------------
+def main():
+    input_path = "./assets/rhyme_annotated.jsonl"
+    output_path = "./assets/rhyme_pairs.csv"
+
+    songs = load_jsonl(input_path)
+    all_pairs = []
+
+    for song in songs:
+        last_words = collect_last_words(song)
+        pairs = generate_pairs(last_words)
+        for w1, w2, label in pairs:
+            features = compute_features(w1, w2)
+            all_pairs.append((*features, label))
+
+    balanced_pairs = check_balance(all_pairs)
+    save_to_csv(balanced_pairs, output_path)
+
+
+
+
+
+### -------------------------------
+# Entry Point
+### -------------------------------
+if __name__ == "__main__":
+    main()
